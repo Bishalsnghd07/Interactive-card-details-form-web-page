fb3714ef9a4c3928d9b49b6827c6e7814bb3e6ee
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-03-20 22:53:10 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,286 @@
-import Image from "next/image";
+'use client';
+import { useState } from "react";
+import Ripples from "react-ripples";
 
 export default function Home() {
+  const [cardholderNameError, setCardholderNameError] = useState('');
+  const [cardNumberError, setCardNumberError] = useState('');
+  const [monthError, setMonthError] = useState('');
+  const [yearError, setYearError] = useState('');
+  const [cvcError, setCvcError] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [cardNumber, setCardNumber] = useState('');
+  const [cvc, setCvc] = useState('');
+  const [cardholderName, setCardholderName] = useState('');
+  const [mm, setMm] = useState('');
+  const [yy, setYy] = useState('');
+
+  const validateCardholderName = (cardholderName: string)=> {
+    return cardholderName.length>=1;
+  }
+
+  
+  const validateCardNumber = (cardNumber: string) => {
+    const numbersOnly = /^[0 - 9]+$/;
+
+    if(cardNumber.trim() === '') {
+      setCardNumberError('Card Number cannot be empty');
+    } else if(!numbersOnly.test(cardNumber)) {
+      setCardNumberError('Wrong format, numbers only');
+    } else if(cardNumber.length < 19) {
+      setCardNumberError('Minimum card length should be 16');
+    }
+      else {
+        setCardNumberError('');
+        return true;
+      }
+    }
+    
+    const validateMm = (mm: string) => {
+      const month = parseInt(mm, 10)
+      if (mm.trim()==='') {
+        setMonthError('Cant be blank');
+        return false;
+      } else if (isNaN(month) || month < 1 || month > 12) {
+        setMonthError('Invalid month');
+        return false;
+      } else {
+        setMonthError('');
+        return true;
+      }
+    }
+
+    const validateYy = (yy: string) => {
+      // const year = parseInt(yy, 10)
+      if (yy.trim()==='') {
+        setYearError('Cant be blank');
+        return false;
+      }
+      if (!/^\d{2}$/.test(yy)) {
+        setYearError('Invalid year format!');
+        return false;
+      }
+       else {
+        setYearError('');
+        return true;
+      }
+    }
+
+    const validateCvc = (cvc: string) => {
+      if(cvc.trim() === '') {
+        setCvcError('cant be blank');
+      } else if(cvc.length!==3) {
+        setCvcError('cvc length should be exact 3')
+      } else {
+        setCvcError('');
+        return true;
+      }
+    }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const cardholderNameInput = document.getElementById('cardholderName') as HTMLInputElement;
+    const cardNumberInput = document.getElementById('cardNumber') as HTMLInputElement;
+    const cardMonthInput = document.getElementById('mm') as HTMLInputElement;
+    const cardYearInput = document.getElementById('yy') as HTMLInputElement;
+    const cardCvcInput = document.getElementById('cvc') as HTMLInputElement;
+
+
+    if(validateCardholderName(cardholderNameInput.value)) {
+      setCardholderNameError('');
+    } else {
+      setCardholderNameError('Cardholder name cannot be empty');
+    }
+
+    if(validateCardNumber(cardNumberInput.value)) {
+      setCardNumberError('');
+    } else {
+      return;
+    }
+    if(validateMm(cardMonthInput.value)) {
+      setMonthError('');
+    } else {
+      return;
+    }
+    if(validateYy(cardYearInput.value)) {
+      setYearError('');
+    } else {
+      return;
+    }
+    if(validateCvc(cardCvcInput.value)) {
+      setCvcError('');
+      setFormSubmitted(true);
+    } else {
+      setFormSubmitted(false);
+      return;
+    }
+
+  }
+  const thanksForm = () => {
+    setFormSubmitted(false);
+    setCardholderName('');
+    setCardNumber('');
+    setMm('');
+    setYy('');
+    setCvc('');
+  }  
+
+  const handleCardNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // setCardNumber(event?.target?.value)
+    const input = event.target.value;
+    const formattedInput = input.replace(/\D/g, '')
+     // Remove non-numeric characters
+     .slice(0, 16)
+                               .replace(/(.{4})/g, '$1 '); // Add space after every 4 characters
+                               setCardNumber(formattedInput.trim()); //Removing any trailing space
+  }
+
+  const handleCvcChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCvc(event?.target?.value)
+  }
+
+  const handleCardholderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCardholderName(event?.target?.value)
+  }
+
+  const handleMmChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMm(event?.target?.value)
+  }
+
+  const handleYyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setYy(event?.target?.value)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex relative font-semibold text-xl min-h-screen flex-col items-center justify-center pt-8 pb-12 xl:pt-20 xl:pb-20 pl-4 pr-4 xl:pl-12 xl:pr-12">
+        <img src="/images/bg-main-desktop.png" alt={""} height={100} width={100} className="h-[100%] w-auto bg-center bg-no-repeat left-0 top-0 absolute hidden xl:block" aria-hidden="true"/> 
+        <img src="/images/bg-main-mobile.png" alt={""} className="h-60 bg-center bg-no-repeat absolute xl:hidden block top-0 w-full"/>
+      
+      <div className="grid xl:grid-flow-col-dense gap-4 w-full z-10">
+        <div className="relative flex flex-col-reverse justify-center xl:items-start items-center xl:flex-col xl:pl-24 gap-10">
+
+           {/* Front card */}
+          <div className="flex relative justify-between mr-[3.6rem] mt-[6.12rem] xl:mt-0 xl:mr-0 z-20">
+            <img src="/images/bg-card-front.png" alt={""} className="w-[17rem] h-[10.5rem] xl:w-auto xl:h-auto bg-contain bg-center"/>
+            <img src="/images/card-logo.svg" alt="" className="absolute top-5 left-5 xl:top-8 xl:left-8 xl:w-auto xl:h-auto w-[25%] h-[24%]" />
+            <h1 className="text-white text-lg xl:text-3xl font-medium tracking-wider xl:tracking-widest absolute xl:bottom-[4.2rem] xl:left-8 bottom-[3rem] left-6">{cardNumber ? cardNumber: '0000 0000 0000 0000'}</h1>
+            <p className="absolute text-white bottom-4 left-6 xl:bottom-6 xl:left-8 font-medium capitalize text-sm xl:text-base">{cardholderName ? cardholderName: 'JANE APPLESSED'}</p>
+            <p className="absolute bottom-4 right-6 xl:bottom-6 xl:right-8 text-white text-sm xl:text-base font-medium">{mm ? mm: '00'}/{yy ? yy: '00'}</p>
+          </div>
+
+          {/* Back card */}
+          <div className="absolute top-2 right-0 sm:right-[5rem] md:right-[14rem] lg:right-[20rem] text-center xl:relative xl:flex xl:pl-24 xl:left-0"> 
+          <img src="/images/bg-card-back.png" alt={""} className="w-[18rem] h-[10rem] xl:w-auto xl:h-auto"/>
+          <div className="absolute top-[4.2rem] right-[2rem] xl:right-[3.6rem] xl:top-[6.66rem] text-white text-base text-center xl:text-xl tracking-wider font-semibold">{cvc ? cvc: '000'}</div>
+          </div>
         </div>
+        {!formSubmitted && (
+        <div className="flex justify-center xl:justify-start items-center">
+        <form id="form" className="max-w-[27rem] pl-[0] xl:pl-0 pr-[0] xl:pr-0 pt-[2rem]" onSubmit={handleSubmit} noValidate>
+  <div className="mb-5">
+    <label htmlFor="cardholderName" className="flex mb-1 text-left text-sm font-semibold tracking-widest text-gray-900 leading-6">CARDHOLDER NAME
+    </label>
+    <input 
+      type="text" 
+      id="cardholderName" 
+      className={`bg-gray-50 border border-gray-200 text-base rounded-lg text-gray-900 
+focus:outline-none w-full px-6 p-3`} 
+      placeholder="e.g. Jane Applessed"  
+      value={cardholderName}
+      onChange={handleCardholderChange}
+      />
+      {cardholderNameError && <p className="mt-2 text-red-600 text-sm">{cardholderNameError}</p>}
+  </div>
+  <div className="mb-5">
+    <label htmlFor="cardNumber" className="flex mb-1 text-left text-sm font-semibold tracking-widest  text-gray-900 leading-6">CARD NUMBER
+    </label>
+    <input 
+      type="text" 
+      id="cardNumber" 
+      className={`bg-gray-50 border border-gray-200 text-base rounded-lg text-gray-900 
+focus:outline-none w-full px-6 p-3`} 
+      placeholder="e.g. 1234 5678 9132 0000"  
+      value={cardNumber}
+      onChange={handleCardNumberChange}
+      />
+      {cardNumberError && <p className="mt-2 text-red-600 text-sm">{cardNumberError}</p>}
+  </div>
+  <div className="mb-12">
+    <div className="grid grid-cols-2 gap-6">
+      <div>
+    <label htmlFor="cardholderName" className="flex mb-1 text-left text-sm font-semibold tracking-normal xl:tracking-widest text-gray-900 leading-6">EXP. DATE (MM/YY)
+    </label>
+    <div className="grid grid-cols-2 gap-4">
+      {/* <div className="relative p-4"> */}
+
+    <input 
+      type="number" 
+      id="mm" 
+      className={`bg-gray-50 border border-gray-200 text-base rounded-lg text-gray-900 
+focus:outline-none w-full px-4 xl:px-6 p-3`} 
+      placeholder="MM"  
+      value={mm}
+      onChange={handleMmChange}
+      />
+       <input 
+      type="text" 
+      id="yy" 
+      className={`bg-gray-50 border border-gray-200 text-base rounded-lg text-gray-900 
+      focus:outline-none w-full px-5 xl:px-6 p-3`} 
+      placeholder="YY"  
+      value={yy}
+      onChange={handleYyChange}
+      />
+      {/* </div> */}
+    </div>
+      <div className="flex w-full">
+      {monthError ? <p className="flex pt-1 justify-start text-red-600 text-sm">{monthError}</p> : (yearError ? <p className="flex justify-end text-red-600 text-sm pb-0 pt-1 pl-[5.5rem] xl:pl-[6.9rem]">{yearError}</p>: null)}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+      <label htmlFor="cardholderName" className="flex mb-1 text-left text-sm font-semibold tracking-widest  text-gray-900 leading-6">CVC
+    </label>
+    <input 
+      type="text" 
+      id="cvc" 
+      className={`bg-gray-50 border border-gray-200 text-base rounded-lg text-gray-900 
+focus:outline-none w-full px-6 p-3`} 
+      placeholder="e.g. 123"  
+      value={cvc}
+      onChange={handleCvcChange}
+      minLength={3}
+      maxLength={3}
+      />
+      {cvcError && <p className="mt-1 text-red-600 text-sm">{cvcError}</p>}
       </div>
+    </div>
+  </div>
+  <Ripples 
+    placeholder="click to interact" 
+    onPointerEnterCapture={undefined} 
+    onPointerLeaveCapture={undefined} 
+    className="w-full" 
+    during={1600}  
+  >
+    <button type="submit" 
+     className="text-white bg-slate-800 hover:bg-slate-700 font-semibold rounded-lg text-lg md:text-base w-full py-4 text-center transition duration-500 shadow-md hover:bg-gradient-to-r from-slate-900 to-slate-600 ease-linear"
+    >
+      Confirm
+    </button>
+  </Ripples>
+  </form>
+      </div>
+        )} {formSubmitted && (
+          <div className="grid xl:grid-flow-col-dense gap-4 w-full xl:pr-[14rem]">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <img src="/images/icon-complete.svg" alt=""  className="pb-4"/>
+          <h1 className="text-2xl font-semibold text-black tracking-widest text-center">THANK YOU!</h1>
+          <p className="text-base font-medium text-center">We have added your card details</p>
+           <button type="button" onClick={thanksForm} className={`w-full max-w-[22rem] sm:w-[22rem] xl:w-[21rem] my-6 text-white bg-slate-900 py-2.5 rounded-lg px-2 text-center`}>Continue</button>
+           </div>
+          </div>
+        )}
+        </div>
     </main>
   );
 }
